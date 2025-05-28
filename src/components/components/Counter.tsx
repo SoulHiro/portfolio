96137ff93c9e688e0236e2478b043439ed803a84
Victor M. <114775078+SoulHiro@users.noreply.github.com>
2025-05-28 11:10:45 -0300

    Update Counter.tsx

--- src/components/components/Counter.tsx
+++ src/components/components/Counter.tsx
@@ -1,30 +1,26 @@
-// /components/Portfolio/components/Counter.tsx
 "use client";
 import React, { useState, useEffect } from 'react';
 
 interface SectionCounterProps {
-  totalSections: number;  // Número total de seções
+  totalSections: number;
 }
 
 const SectionCounter: React.FC<SectionCounterProps> = ({ totalSections }) => {
   const [activeSection, setActiveSection] = useState(1);
 
   useEffect(() => {
-    // Configura os observadores para cada seção
     const sectionObservers: IntersectionObserver[] = [];
     const options: IntersectionObserverInit = {
-      root: null, // usando a viewport como elemento de referência
-      rootMargin: '-50% 0px', // considera o elemento visível quando está pelo menos 50% na tela
-      threshold: 0 // dispara quando qualquer parte do elemento está visível
+      root: null,
+      rootMargin: '-50% 0px',
+      threshold: 0
     };
 
-    // Observer callback
     const handleIntersect = (
       entries: IntersectionObserverEntry[]
     ): void => {
       entries.forEach((entry: IntersectionObserverEntry) => {
         if (entry.isIntersecting) {
-          // Extrai o número da seção do ID do elemento
           const sectionId = entry.target.id;
           const sectionNumber = parseInt(sectionId.replace('section-', ''));
           setActiveSection(sectionNumber);
@@ -32,7 +28,6 @@ const SectionCounter: React.FC<SectionCounterProps> = ({ totalSections }) => {
       });
     };
 
-    // Cria um observador para cada seção
     for (let i = 1; i <= totalSections; i++) {
       const sectionElement = document.getElementById(`section-${i}`);
       if (sectionElement) {
@@ -42,7 +37,6 @@ const SectionCounter: React.FC<SectionCounterProps> = ({ totalSections }) => {
       }
     }
 
-    // Limpa os observadores quando o componente é desmontado
     return () => {
       sectionObservers.forEach((observer: IntersectionObserver) => observer.disconnect());
     };
